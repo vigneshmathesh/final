1220166ee439aa3e1d7e815c9a3b93f01c6cecbf
Vignesh M <vickysakthi965@gmail.com>
2025-07-29 17:31:38 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,5 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
     return "🚀 Flask App Deployed with Terraform and GitHub Actions!"
+
+# ✅ This part is critical
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
